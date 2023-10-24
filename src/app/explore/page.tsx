2f6605c5a7fe9c0a6790b16913b968ec2a137df6
NeverLost-Never <haileyesus.mieron@gmail.com>
2023-10-23 22:42:49 -0400

    Pages - Explore (created)

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,14 @@
+import "./_page.scss";
+
+// COMPONENTS
+import Navbar from "@/components/Navbar/navbar";
+
+const Explore = () => {
+	return (
+		<div className="explore">
+			<Navbar />
+		</div>
+	);
+};
+
+export default Explore;
